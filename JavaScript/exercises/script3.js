bf5e4ae162322c8d0e7ef857e3a56a0ba40da0d2
Emanuel S. Pedrozo <mr.espedrozo@gmail.com>
2021-05-19 17:33:25 -0300

    exercices update

---
+++ JavaScript/exercises/script3.js
@@ -0,0 +1,10 @@
+/* 
+    Celsius em Fahrenheit
+
+    Crie uma função que receba uma stirng em celsius ou fahrenheit e faça a transformação de uma unidade para outra.
+
+        C = (F - 32) * 5/9
+
+        F = C * 5/9 + 32
+
+*/
