f61f218140337cedc6c912a7bcdcf783048865f8
Emanuel S. Pedrozo <mr.espedrozo@gmail.com>
2021-05-21 23:41:54 -0300

    last exercise js

--- JavaScript/exercises/script4.js
+++ JavaScript/exercises/script4.js
@@ -1,4 +1,8 @@
 /*
-    Baseado no Arrays
-
+    Baseado no Array de livros por categoria abaixo, faça os seguintes desafios
+        - contar o número de categorias e o número de livros em cada categoria
+        - contar o número de autores
+        - mostrar livros do autor Augusto Cury
+        - transformar a função acima em uma função que irá receber o nome do autor
+            e devolver os livros desse autor.
 */    
