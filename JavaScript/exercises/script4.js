d8a39240c0ab9c5c7948d01a14762ff263a97eca
Emanuel S. Pedrozo <mr.espedrozo@gmail.com>
2021-05-22 23:00:07 -0300

    update exercises

--- JavaScript/exercises/script4.js
+++ JavaScript/exercises/script4.js
@@ -6,3 +6,42 @@
         - transformar a função acima em uma função que irá receber o nome do autor
             e devolver os livros desse autor.
 */    
+
+
+
+const  booksByCategory = [
+    {
+        category: "Riqueza",
+        books: [
+            {
+                title: "Os segredos da mente milionária",
+                author: "T. Harv Eker",
+            },
+            {
+                title: "O homem mais rico da Babilônia",
+                author: "George S. Clason",
+            },
+            {
+                title: "Pai rico, pai pobre",
+                author: "Robert T. Kiyosaki e Sharon L. Lechter",
+            },
+        ],
+    },
+    {
+        category: "Inteligência Emocional",
+        books: [
+            {
+                title: "Você é Insubstituível",
+                author: "Augusto Cury",
+            },
+            {
+                title: "Ansiedade - Como enfrentar o mal do século",
+                author: "Augusto Cury",
+            },
+            {
+                title: "Os 7 hábitos de pessoas altamente eficazes",
+                author: "Stephen R. Covey",
+            },
+        ],
+    },
+];
